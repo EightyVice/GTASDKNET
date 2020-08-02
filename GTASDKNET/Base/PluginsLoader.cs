70ad40992e7e9b92ef2a31d94390273153e3f367
uwx <cadrekucra@gmx.com>
2020-08-02 01:20:18 -0300

    Switch to Roslyn for dynamic plugin compilation

--- GTASDKNET/Base/PluginsLoader.cs
+++ GTASDKNET/Base/PluginsLoader.cs
@@ -1,92 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
 using System.IO;
-using System.CodeDom.Compiler;
+using System.Diagnostics;
 using System.Windows.Forms;
+using GTASDK.Base;
 
 namespace GTASDK
 {
-    class PluginsLoader
+    internal class PluginsLoader
     {
-        public List<Assembly> Assemblies;
-        public List<Type> Classes;
+        public IList<Assembly> Assemblies = new List<Assembly>();
+        public IList<Type> Classes = new List<Type>();
 
-        private string[] _cmdLine;
+        private readonly string[] _cmdLine;
+
         public PluginsLoader(string directory, string[] commandLine)
         {
-
             _cmdLine = commandLine;
-            Assemblies = new List<Assembly>();
-            foreach (string file in Directory.GetFiles(directory))
+            try
             {
-                if (file.EndsWith(".net.dll"))
+                Parallel.ForEach(Directory.GetFiles(directory), file =>
                 {
-                    Assembly assembly = Assembly.LoadFrom(file);
-                    Assemblies.Add(assembly);
-                }
-                if (file.EndsWith(".cs") || file.EndsWith(".vb"))
-                {
-                    var compilerParams = new CompilerParameters();
-                    compilerParams.CompilerOptions = "/optimize";
-                    compilerParams.GenerateInMemory = true;
-                    compilerParams.IncludeDebugInformation = true;
-                    compilerParams.ReferencedAssemblies.Add("System.dll");
-                    compilerParams.ReferencedAssemblies.Add("System.Core.dll");
-                    compilerParams.ReferencedAssemblies.Add("System.Drawing.dll");
-                    compilerParams.ReferencedAssemblies.Add("System.Windows.Forms.dll");
-                    compilerParams.ReferencedAssemblies.Add("System.XML.dll");
-                    compilerParams.ReferencedAssemblies.Add("System.XML.Linq.dll");
-                    compilerParams.ReferencedAssemblies.Add("VCScriptHook.dll");
-
-                    CodeDomProvider compiler = null;
-                    if (file.EndsWith(".cs", StringComparison.OrdinalIgnoreCase))
+                    switch (Path.GetExtension(file))
                     {
-                        compiler = new Microsoft.CSharp.CSharpCodeProvider();
-                        //compilerParams.CompilerOptions += "/unsafe";
+                        case ".net.dll":
+                            Assemblies.Add(Assembly.LoadFrom(file));
+                            break;
+                        case ".cs":
+                            Assemblies.Add(RoslynCompiler.CompileCSharpToAssembly(File.ReadAllText(file), file));
+                            break;
+                        case ".vb":
+                            Assemblies.Add(RoslynCompiler.CompileVbToAssembly(File.ReadAllText(file), file));
+                            break;
                     }
-                    if (file.EndsWith(".vb", StringComparison.OrdinalIgnoreCase))
-                    {
-                        compiler = new Microsoft.VisualBasic.VBCodeProvider();
-                    }
-
-                    CompilerResults compilerResults = compiler.CompileAssemblyFromFile(compilerParams, file);
-                    if (!compilerResults.Errors.HasErrors)
-                    {
-                        Assemblies.Add(compilerResults.CompiledAssembly);
-                    }
-                    else
-                    {
-                        StringBuilder sb = new StringBuilder();
-                        foreach (CompilerError err in compilerResults.Errors)
-                        {
-                            sb.AppendLine(err.ErrorText);
-                        }
-                        MessageBox.Show(sb.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    }
-                }
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Errored during assembly loading or compilation:\n" + ex, "GTASDK.NET", MessageBoxButtons.OK);
+                Debugger.Break();
+                Environment.Exit(1);
             }
         }
         public void InitAllScripts()
         {
-            List<Type> Classes = new List<Type>();
-            foreach (Assembly asm in Assemblies)
-            {
-                // Check all classes in the script
-                foreach (Type cls in asm.GetTypes())
-                {
-                    // If the class inherits base
-                    if (cls.BaseType == typeof(Plugin))
-                    {
-                        Classes.Add(cls);
-                    }
-                }
-            }
+            var classes =
+                from asm in Assemblies
+                from cls in asm.GetTypes()
+                where cls.BaseType == typeof(Plugin)
+                select cls;
 
-            foreach (Type script in Classes)
+            foreach (var script in classes)
             {
                 // Search for all constructors in the script.
                 foreach (var ctor in script.GetConstructors())
